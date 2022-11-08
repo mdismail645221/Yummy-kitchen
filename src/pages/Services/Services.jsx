316c43985894d538305c6b551fac2205372b5a3a
mdismail645221 <mdismail645221@gmail.com>
2022-11-09 03:01:41 +0600

    reviews details is send the database stored

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react';
+import toast from 'react-hot-toast';
 import { Link, useLoaderData } from 'react-router-dom';
 import { AuthConext } from '../../context/AuthProvider';
 
@@ -13,14 +14,49 @@ const Services = () => {
 
 
     // console.log(userReviewMsg)
-    const reviews = {
-        serviceInfo: userReviewMsg,
-        useEmail: user?.email,
-        userName: user?.displayName,
-        serviceId : _id,
-        userImg: user?.photoURL,
-    }
     // console.log(reviews)
+    
+    
+    const handleReviewSubmit = (event) => {
+        event.preventDefault()
+
+        console.log()
+
+        
+        const allReviews = {
+            serviceInfo: userReviewMsg,
+            useEmail: user?.email,
+            userName: user?.displayName,
+            serviceId : _id,
+            userImg: user?.photoURL,
+        }
+
+
+        if(userReviewMsg.length <= 40 && userReviewMsg.length <= 0){
+            toast.error('please added the 40 characters.')
+            return
+        }else{
+            toast.success('Successfully added your revies. GOOD JOB')
+            event.target.form.reset()
+        }
+
+        // console.log(allReviews)
+
+        fetch(`http://localhost:5000/allReviews`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(allReviews)
+        })
+        .then(res=> res.json())
+        .then(data => {
+            console.log(data)
+        })
+
+
+
+    }
 
 
 
@@ -53,19 +89,19 @@ const Services = () => {
                     {
                         user ?
                             <>
-                                <form className="flex flex-col  shadow-sm rounded-xl mb-20 dark:bg-gray-900 dark:text-gray-100">
+                                <form className="flex flex-col  shadow-sm rounded-xl mb-20 dark:bg-gray-900 dark:text-gray-100 form-control">
                                     <div className="flex flex-col items-center w-full">
                                         <h2 className="text-3xl font-semibold text-center my-5">Your opinion Reviews!</h2>
-                                        <div className="flex flex-col w-4/5 mx-auto">
-                                            <textarea onChange={(e)=> setUserReviewMsg(e.target.value)} rows="5" className="textarea textarea-warning" placeholder="Write Your Review"></textarea>
-                                            <button type="button" className="inline-block W-20  py-4 my-8 font-semibold rounded-md dark:text-gray-900 btn-primary btn">Send feedback</button>
+                                        <div className="flex flex-col w-4/5 mx-auto form-control">
+                                            <textarea onChange={(e)=> setUserReviewMsg(e.target.value)} name='name'  rows="5" className="textarea textarea-warning" placeholder="Write Your Review" required />
+                                            <button onClick={handleReviewSubmit} type='submit' className="inline-block W-20  py-4 my-8 font-semibold rounded-md dark:text-gray-900 btn-primary btn">Send feedback</button>
                                         </div>
                                     </div>
                                 </form>
                             </>
                             :
                             <>
-                                <div className='flex justify-center pt-10 pb-20'>
+                                <div  className='flex justify-center pt-10 pb-20'>
                                     <p className='text-3xl'>Please <Link to='/login' className='primary-color'>login</Link> to add a review</p>
                                 </div>
                             </>

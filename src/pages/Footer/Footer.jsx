ed4e7c5bd66f1b86b3e748ba96a4a3b45fe9d738
mdismail645221 <mdismail645221@gmail.com>
2022-11-08 18:52:16 +0600

    footer some text added

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,65 +1,53 @@
 import React from 'react';
+import logo from '../../assets/images/favicon.ico'
 
 const Footer = () => {
     return (
-        <footer className="px-4 divide-y dark:bg-gray-800 dark:text-gray-100">
-            <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+        <footer className="px-4 divide-y dark:bg-gray-800 dark:text-gray-100 primary-bg-color">
+            <div className="container flex mx-auto flex-col justify-between py-10  space-y-8 lg:flex-row lg:space-y-0">
                 <div className="lg:w-1/3">
                     <a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
                         <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-900">
-                                <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
-                            </svg>
+                            <img src={logo} alt="logo" />
                         </div>
-                        <span className="self-center text-2xl font-semibold">Brand name</span>
+                        <span className="self-center text-2xl font-semibold">YUMMY KITCHEN</span>
                     </a>
                 </div>
                 <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
                     <div className="space-y-3">
-                        <h3 className="tracking-wide uppercase dark:text-gray-50">Product</h3>
+                        <h3 className="tracking-wide uppercase dark:text-gray-50 text-1xl font-bold">Product</h3>
                         <ul className="space-y-1">
                             <li>
-                                <a rel="noopener noreferrer" href="#">Features</a>
+                                <a rel="noopener noreferrer" href="#">Home</a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">Integrations</a>
+                                <a rel="noopener noreferrer" href="#">Service</a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">Pricing</a>
+                                <a rel="noopener noreferrer" href="#">My Reviews</a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">FAQ</a>
+                                <a rel="noopener noreferrer" href="#">Blog</a>
                             </li>
                         </ul>
                     </div>
                     <div className="space-y-3">
-                        <h3 className="tracking-wide uppercase dark:text-gray-50">Company</h3>
+                        <h3 className="uppercase dark:text-gray-50 text-1xl font-bold">Developers</h3>
                         <ul className="space-y-1">
                             <li>
-                                <a rel="noopener noreferrer" href="#">Privacy</a>
-                            </li>
-                            <li>
-                                <a rel="noopener noreferrer" href="#">Terms of Service</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="space-y-3">
-                        <h3 className="uppercase dark:text-gray-50">Developers</h3>
-                        <ul className="space-y-1">
-                            <li>
-                                <a rel="noopener noreferrer" href="#">Public API</a>
+                                <a rel="noopener noreferrer" href="#">MongoDB API</a>
                             </li>
                             <li>
                                 <a rel="noopener noreferrer" href="#">Documentation</a>
                             </li>
                             <li>
-                                <a rel="noopener noreferrer" href="#">Guides</a>
+                                <a rel="noopener noreferrer" href="#">React</a>
                             </li>
                         </ul>
                     </div>
                     <div className="space-y-3">
-                        <div className="uppercase dark:text-gray-50">Social media</div>
-                        <div className="flex justify-start space-x-3">
+                        <div className="uppercase dark:text-gray-50 text-1xl font-bold">Social media</div>
+                        <div className="flex justify-center space-x-3">
                             <a rel="noopener noreferrer" href="#" title="Facebook" className="flex items-center p-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                                     <path d="M32 16c0-8.839-7.167-16-16-16-8.839 0-16 7.161-16 16 0 7.984 5.849 14.604 13.5 15.803v-11.177h-4.063v-4.625h4.063v-3.527c0-4.009 2.385-6.223 6.041-6.223 1.751 0 3.584 0.312 3.584 0.312v3.937h-2.021c-1.984 0-2.604 1.235-2.604 2.5v3h4.437l-0.713 4.625h-3.724v11.177c7.645-1.199 13.5-7.819 13.5-15.803z"></path>
@@ -79,7 +67,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="py-6 text-sm text-center dark:text-gray-400">© 1968 Company Co. All rights reserved.</div>
+            <div className="py-6 text-sm text-center dark:text-gray-400">© 2022 Company Co. All rights reserved.</div>
         </footer>
     );
 };
--- src/pages/shared/Sliders/Slider.jsx
+++ src/pages/shared/Sliders/Slider.jsx
@@ -16,9 +16,9 @@ const Slider = () => {
                     <a href="#slide4" className="btn btn-circle bg-orange-500 ">❮</a>
                     <a href="#slide2" className="btn btn-circle bg-orange-500 ">❯</a>
                 </div>
-                <div className="card w-1/2 text-left absolute top-1/3 left-10">
+                <div className="card w-full md:w-1/2 text-left absolute left-10" style={{top: "50px"}}>
                     <div className="card-body">
-                        <h2 className="card-title text-5xl text-white font-bold">BINNI CHALER PITHA</h2>
+                        <h2 className="card-title text-2xl md:text-5xl text-white font-bold">BINNI CHALER PITHA</h2>
                         <p className='text-gray-50 text-3xl font-semibold'>Binni rice pita is one of the favorite foods in winter. Before the sun rises in the morning, mothers</p>
                         <div className="card-actions my-3">
                             <button className="btn px-10 py-4 btn-primary">SUBSCRIBES NOW!</button>
@@ -34,9 +34,9 @@ const Slider = () => {
                     <a href="#slide1" className="btn btn-circle bg-orange-500">❮</a>
                     <a href="#slide3" className="btn btn-circle bg-orange-500">❯</a>
                 </div>
-                <div className="card w-1/2 text-left absolute top-1/3 left-10">
+                <div className="card w-full md:w-1/2 text-left absolute left-10" style={{top: "50px"}}>
                     <div className="card-body">
-                        <h2 className="card-title text-5xl text-white font-bold">PIZZA</h2>
+                        <h2 className="card-title text-2xl md:text-5xl text-white font-bold">PIZZA</h2>
                         <p className='text-gray-50 text-3xl font-semibold'>pizza is very interesting fast food. tasty .</p>
                         <div className="card-actions my-3">
                             <button className="btn px-10 py-4 btn-primary">SUBSCRIBES NOW!</button>
@@ -52,9 +52,9 @@ const Slider = () => {
                     <a href="#slide2" className="btn btn-circle bg-orange-500">❮</a>
                     <a href="#slide4" className="btn btn-circle bg-orange-500">❯</a>
                 </div>
-                <div className="card w-1/2 text-left absolute top-1/3 left-10">
+                <div className="card w-full md:w-1/2 text-left absolute left-10" style={{top: "50px"}}>
                     <div className="card-body">
-                        <h2 className="card-title text-5xl text-white font-bold">PULI PITHA</h2>
+                        <h2 className="card-title text-2xl md:text-5xl text-white font-bold">PULI PITHA</h2>
                         <p className='text-gray-50 text-3xl font-semibold'>Paush-Parban, the festival of Pithe-Puli of Bengali, has just passed. On the auspicious occasion of .</p>
                         <div className="card-actions my-3">
                             <button className="btn px-10 py-4 btn-primary">SUBSCRIBES NOW!</button>
@@ -70,9 +70,9 @@ const Slider = () => {
                     <a href="#slide3" className="btn btn-circle bg-orange-500">❮</a>
                     <a href="#slide1" className="btn btn-circle bg-orange-500">❯</a>
                 </div>
-                <div className="card w-1/2 text-left absolute top-1/3 left-10">
+                <div className="card w-full md:w-1/2 text-left absolute left-10" style={{top: "50px"}}>
                     <div className="card-body">
-                        <h2 className="card-title text-5xl text-white font-bold">CHITOI PITHA</h2>
+                        <h2 className="card-title text-2xl md:text-5xl text-white font-bold">CHITOI PITHA</h2>
                         <p className='text-gray-50 text-3xl font-semibold'>My favourite combination, however, is the chingri shutki and morich bhorta. The dried shrimp flavour....</p>
                         <div className="card-actions my-3">
                             <button className="btn px-10 py-4 btn-primary">SUBSCRIBES NOW!</button>
